37fca9fc6b0054b6c08a24b864d8e6e2ada6cb09
DarioCaptain <daapplab@gmail.com>
2020-02-09 22:54:02 +0100

    Various
    *Fixed issues
    *moving a block edits the matrix where all block positions are entered

--- godotProject/scripts/game.gd
+++ godotProject/scripts/game.gd
@@ -5,11 +5,44 @@ extends Spatial
 onready var block = preload("res://scenes/block.tscn")
 
 #the block
-var block_inst
+var blocks = []
+
+#the board length
+var boardLength
+#the maximum height
+var maxHeight = 5
+#the board as matrix
+var board = []
 
 
 func _ready():
 
-	block_inst = block.instance()
-	add_child(block_inst)
+	#get the length of the ground (board length)
+	boardLength = get_node("ground").scale.x
 
+	#initialize the board-matrix
+	for x in range(boardLength * 2):
+		board.append([])
+		for y in range(maxHeight):
+			board[x].append(0)
+
+
+	#GET RANDOM BLOCKS AND COORDINATES
+
+
+	#instantiate the random blocks
+	for y in range(5):
+		#instantiate the block 
+		var block_inst = block.instance()
+		add_child(block_inst)
+		block_inst.translate(Vector3(0, y * 2, 0))
+		board[0][y] = 1
+
+		#add the block to the list with all blocks
+		blocks.append([y, block_inst])
+
+
+
+func generatePuzzle():
+
+	pass
